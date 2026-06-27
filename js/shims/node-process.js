5c0eb962946cf6bc743f529e9077ac6e390be3e1
dxkite <dxkite@163.com>
2026-06-27 20:16:27 +0800

    feat: shims

--- js/shims/node-process.js
+++ js/shims/node-process.js
@@ -1,7 +1,9 @@
+export const cwd = () => '/';
 export const env = globalThis.__processEnv || {};
 export const version = 'v20.0.0';
 export const versions = {};
 export const platform = 'linux';
 export const stdout = { fd: 1, write: () => {} };
 export const stderr = { fd: 2, write: () => {} };
-export default globalThis.process || { env, version, versions, platform, stdout, stderr };
+const _proc = globalThis.process || {};
+export default { cwd, env, version, versions, platform, stdout, stderr, ..._proc };
